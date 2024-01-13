81fbc07d34a026fdf3bdf10bf249081809e64cd4
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2024-01-13 14:44:04 +0100

    cration de la pagination

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,29 +2,32 @@
 import { ref, onMounted } from "vue";
 import DefaultLayout from '@/components/layouts/DefaultLayout.vue'
 
-import {instance } from "@/utils/axios";
+import {instance } from "@/utils/axios.js";
+
 const getMontres = async () => {
   const reponse = await instance.get("/Montres");
   return reponse.data;
 };
 console.log(getMontres());
-const montres = ref();
+const Montres = ref([]);
 onMounted(async () => {
-  montres.value = await getMontres();
-  console.log(montres.value);
+  Montres.value = await getMontres();
+  console.log(Montres.value);
 });
 
 </script>
 <template>
   <DefaultLayout>
     <template #header>  </template>
-    Page about
+    <div v-for="montre in Montres" :key="montre.id_Montre">
+          <!-- <p>{{montre.id_boitier}}</p>
+          <p>{{montre.id_bracelet}}</p>
+          <p>{{montre.Boitiers_Form}}</p> -->
+
+        </div>   
     <template #aside> 
         <!-- fait une boucle v-for -->
-        <div v-for="montre in montres" :key="montre.id">
-          <h2>{{ montre.Nom_Montre }}</h2>
-          <p>{{ montre.id_boitier }}</p>
-        </div>
+         
     </template>
     <template #footer> </template>
   </DefaultLayout>
--- src/views/InspirationsView.vue
+++ src/views/InspirationsView.vue
@@ -1,30 +1,48 @@
 <script setup>
 import DefaultLayout from '@/components/layouts/DefaultLayout.vue';
 import MyCard from '@/components/elements/MyCard.vue'
-import {instance} from '@/utils/axios'
+import { instance } from '@/utils/axios'
 import { ref, onMounted } from "vue";
 
-const getMontres = async()=>{
+const getMontres = async () => {
   const response = await instance.get('/Montres');
   return response.data;
 }
-const montres = ref();
 
-onMounted(async()=>{
+const montres = ref([]);
+const currentPage = ref(1);
+const cardsPerPage = 6;
+
+const getCardRange = () => {
+  const startIndex = (currentPage.value - 1) * cardsPerPage;
+  const endIndex = startIndex + cardsPerPage;
+  return { startIndex, endIndex };
+};
+
+onMounted(async () => {
   montres.value = await getMontres();
 })
 </script>
 
 <template>
-    <DefaultLayout>
-        <div>
-    <h1>Liste des Montres</h1>
-    <div v-for="montre in montres" :key="montre.id_montre">
-      <MyCard :boitier_image_url="montre.id_boitier" :bracelet_image_url="montre.id_bracelet" :Boitiers_Form="montre.Boitiers_Form"/>
+  <DefaultLayout>
+    <div>
+      <h1>Liste des Montres</h1>
+      <div v-for="montre in montres && montres.length ? montres.slice(getCardRange().startIndex, getCardRange().endIndex) : []" :key="montre.id_montre">
+        <MyCard :boitier_image_url="montre.id_boitier" :bracelet_image_url="montre.id_bracelet" :Boitiers_Form="montre.Boitiers_Form"/>
+        <p>{{ montre.Prix_unitaire }}</p>
+        <p>{{montre.id_bracelet}}</p>
+      </div>
+      
+      <div>
+        <button @click="currentPage -= 1" :disabled="currentPage === 1">Précédent</button>
+        <span>{{ currentPage }}</span>
+        <button @click="currentPage += 1" :disabled="!montres || getCardRange().endIndex >= montres.length">Suivant</button>
+      </div>
     </div>
-  </div>
-    </DefaultLayout>
+  </DefaultLayout>
 </template>
+
 
 <style lang="scss" scoped>
 
