368e9ca8f208de545e5558165408b8d5ec93b26e
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2024-01-13 13:53:23 +0100

    ajoute la page montre et login dans index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,18 +2,20 @@ import { createRouter, createWebHistory,  } from 'vue-router'
 
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
-// import LoginView from '../views/LoginView.vue'
+import LoginView from '../views/LoginView.vue'
 import IncriptionView from '../views/InscriptionView.vue'
 import InspirationsView from '../views/InspirationsView.vue'
+import MontreView from '../views/Utilisateurs/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {path: '/',              name: 'home',               component: HomeView},
     {path: '/about',         name: 'about',              component: AboutView},
-    // {path: '/login',         name: 'login',              component: LoginView},
+    {path: '/login',         name: 'login',              component: LoginView},
     {path: '/inscription',   name: 'inscription',        component: IncriptionView},
     {path: '/inspirations',  name: 'inspirations',       component: InspirationsView},
+    {path: '/montre',        name: 'montre',             component: MontreView},
 
 
   ]
